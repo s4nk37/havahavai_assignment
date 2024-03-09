deacf9f02b77c65361a9a176a44cde2176aaaab4
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-03-09 15:43:36 +0530

    Initial commit

---
+++ lib/utils/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF080808);
+  static const Color lightGrey = Color(0xFFEEEEEE);
+  static const Color lightGrey2 = Color(0xFFE4E4E4);
+  static const Color lightGrey3 = Color(0xFFDCDCDC);
+  static const Color darkGrey = Color(0xFF767676);
+  static const Color mediumGrey = Color(0xFF909090);
+  static const Color black2 = Color(0xFF222222);
+  static const Color goldenYellow = Color(0xFFCFA92D);
+
+  static const Color blue = Color(0xFF286BF9);
+  static const Color darkBlue = Color(0xFF01000F);
+}
